5a01554d0a2fee0db2656731203cd95c62dde8a4
jimbombo-beep <123792564+jimbombo-beep@users.noreply.github.com>
2023-01-28 13:41:48 +0100

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mbild.mbwatch"},{"l":"org.mbild.mbwatch.app"},{"l":"org.mbild.mbwatch.fx"},{"l":"org.mbild.mbwatch.fx.events"},{"l":"org.mbild.mbwatch.tests"}];updateSearchResults();
