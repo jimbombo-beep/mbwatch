9919abb8e5f192f4a162be1c49bdb5dc9e651bbd
Matthias Bild <123792564+jimbombo-beep@users.noreply.github.com>
2023-02-01 09:26:16 +0100

    Add files via upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mbild.mbwatch"},{"l":"org.mbild.mbwatch.app"},{"l":"org.mbild.mbwatch.fx"},{"l":"org.mbild.mbwatch.fx.events"},{"l":"org.mbild.mbwatch.tests"}];updateSearchResults();
